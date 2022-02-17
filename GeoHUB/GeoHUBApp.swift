53269cad63de6c45cbc7bdca5adbe97a5c5c880e
Maksim Kalik <maksim.kalik@citadele.lv>
2022-02-17 11:30:06 +0200

    Initial Commit

---
+++ GeoHUB/GeoHUBApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoHUBApp.swift
+//  GeoHUB
+//
+//  Created by Maksim Kalik on 17/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct GeoHUBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
